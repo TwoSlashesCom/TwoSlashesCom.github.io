d145f5c9ce862efdf9404b18fbb1f42e7f5b749a
Constintine <75585816+TwoSlashesCom@users.noreply.github.com>
2025-04-18 23:34:56 +0300

    lab05

---
+++ src/mock/task.js
@@ -0,0 +1,55 @@
+export const boardTasks = [
+	{
+		id: '1',
+		title: 'Выучить JS',
+		status: 'done',
+	},
+	{
+		id: '2',
+		title: 'Выучить React',
+		status: 'done',
+	},
+	{
+		id: '3',
+		title: 'Сделать домашку',
+		status: 'done',
+	},
+
+	{
+		id: '4',
+		title: 'Выпить смузи',
+		status: 'in-progress',
+	},
+	{
+		id: '5',
+		title: 'Выпить воды',
+		status: 'in-progress',
+	},
+
+	{
+		id: '6',
+		title: 'Позвонить маме',
+		status: 'done',
+	},
+	{
+		id: '7',
+		title: 'Погладить кота',
+		status: 'done',
+	},
+
+	{
+		id: '8',
+		title: 'Выучить JS',
+		status: 'removed',
+	},
+	{
+		id: '9',
+		title: 'Выучить React',
+		status: 'removed',
+	},
+	{
+		id: '10',
+		title: 'Сделать домашку',
+		status: 'removed',
+	},
+]
